3db20e0db85e0cb8487316b1f57beef83ff3cad6
sarah <sarahmanuela313@gmail.com>
2026-05-05 21:04:37 -0300

    10

--- src/Unidade5/Uni5Exe10.java
+++ src/Unidade5/Uni5Exe10.java
@@ -1,5 +1,16 @@
 package Unidade5;
 
 public class Uni5Exe10 {
-    
+    public static void main(String[] args) {
+        int count = 0;
+        for(int i = 1; count < 10; i++){
+            int esquerda = i / 100;
+            int direita = i % 100;
+
+            if((esquerda + direita) *(esquerda + direita ) == i){
+                System.out.println(i);
+                count++;
+            }
+        }
+    }
 }
