f22e4845680b9d3990d009531e59b1f8b38a3fa7
sarah <sarahmanuela313@gmail.com>
2026-05-24 20:45:17 -0300

    32

---
+++ src/Unidade5/Uni5Exe32.java
@@ -0,0 +1,20 @@
+package Unidade5;
+
+import java.util.Scanner;
+
+public class Uni5Exe32 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int entrada = 0;
+        int saida = 0;
+        System.out.println("Digite o dia da semana que vc quer que comece: ");
+        entrada = sc.nextInt();
+        System.out.println("Digite se quer o mes com 30 ou 31 dias: ");
+        saida = sc.nextInt();
+
+        while (saida ) {
+            
+        }
+    }
+
+}
