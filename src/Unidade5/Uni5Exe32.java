eb44ff5240911f1d93d5745ef51ef48b8e344fb7
sarah <sarahmanuela313@gmail.com>
2026-05-25 20:05:32 -0300

    terminei a lista5

--- src/Unidade5/Uni5Exe32.java
+++ src/Unidade5/Uni5Exe32.java
@@ -12,9 +12,25 @@ public class Uni5Exe32 {
         System.out.println("Digite se quer o mes com 30 ou 31 dias: ");
         saida = sc.nextInt();
 
-        while (saida ) {
-            
+        System.out.println("D\tS\tT\tQ\tQ\tS\tS");
+        int indice = entrada - 1;
+        int espaco = 0;
+
+        while (espaco < indice) {
+            System.out.print("\t");
+            espaco++;// aqui sempre vai pular os espacos
         }
+        int dia = 1;
+        while (dia <= saida) {
+            System.out.print(dia + "\t");
+            indice++;
+            dia++;
+            if (indice == 7) {
+            System.out.println(); 
+            indice = 0; 
+        }
+        }//nunca mais  
+        
     }
 
 }
