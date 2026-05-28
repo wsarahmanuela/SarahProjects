a4cb96b9dbaf434b501a07936429a11c8edbffd7
Sarah Manuela de Carvalho <smcarvalho@furb.br>
2026-05-28 18:38:04 -0300

    naosei

--- src/Unidade5/Uni5Exe27.java
+++ src/Unidade5/Uni5Exe27.java
@@ -5,7 +5,13 @@ import java.util.Scanner;
 public class Uni5Exe27 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int dia;
+        int dia = 1;
+        while (true) {
+            if(dia > 1 || dia < 31){
+                System.out.println("Dia invalido");
+            }
+            
+        }
         
     }
 
