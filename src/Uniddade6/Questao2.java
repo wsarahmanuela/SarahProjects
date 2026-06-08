33d57c1641bec4bdc3901057f63f5f4adb935080
sarah <sarahmanuela313@gmail.com>
2026-06-07 21:56:04 -0300

    jj

--- src/Uniddade6/Questao2.java
+++ src/Uniddade6/Questao2.java
@@ -5,42 +5,59 @@ import java.util.Scanner;
 public class Questao2 {
     private Scanner sc = new Scanner(System.in);
     private int[] numero = new int[10];
-    private int maior = 0;
-    private int menor = 0;
 
-    private void lerNumero(){
-        for(int i = 0; i < 10; i++){
+    private void lerNumero() {
+        for (int i = 0; i < 10; i++) {
             System.out.println("Digite os numeros: ");
             numero[i] = sc.nextInt();
         }
     }
-    private void maiorNumero(){
-        for(int i = 0; i < 10; i++){
-            if(numero[i] > maior){
-                numero[i] = maior;
+
+    private void maiorNumero() {
+        int maior = numero[0];
+        for (int i = 0; i < 10; i++) {
+            if (numero[i] > maior) {
                 maior = numero[i];
             }
         }
-        System.out.println("O maior numero é: "+maior);
+        System.out.println("O maior numero é: " + maior);
     }
-    private void menorNumero(){
-        for(int i = 0; i < 10; i++){
-            if(numero[i] < menor){
-                numero[i] = menor;
+
+    private void menorNumero() {
+        int menor = numero[0];
+        for (int i = 0; i < 10; i++) {
+            if (numero[i] < menor) {
                 menor = numero[i];
             }
         }
-        System.out.println("O menor numero é: "+menor);
+        System.out.println("O menor numero é: " + menor);
     }
-    private void somaNumeros(){
+
+    private int somaNumeros() {
         int soma = 0;
-        for(int i = 0; i < 10; i++){
+        for (int i = 0; i < 10; i++) {
             soma += numero[i];
         }
-        System.out.println("A soma dos numeros é: "+soma);
+        System.out.println("A soma dos numeros é: " + soma);
+        return soma;
     }
-    private void mediaNumeros(){
-        
+
+    private void mediaNumeros() {
+        int soma = somaNumeros();
+        int media = soma / 10; // usa a variável, não chama de novo
+        System.out.println("A media é: " + media);
+    }
+
+    public Questao2() {
+
+    }
+
+    public static void main(String[] args) {
+        Questao2 questao2 = new Questao2();
+        questao2.lerNumero();
+        questao2.maiorNumero();
+        questao2.menorNumero();
+        questao2.mediaNumeros();
     }
 
 }
